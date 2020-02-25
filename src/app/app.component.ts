effb610c4a9e65fe9b8d088c7a1c988701800011
Niraw Chawda <nirawc@obsessory.com>
2020-02-26 01:41:16 +0530

    Integrated Questionairs and implemented the UI with Proper Bar Chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,133 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { AppService } from './app.service';
+import { FormBuilder , FormControl , Validators} from '@angular/forms';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'quizGuru1';
+export class AppComponent implements OnInit {
+	title = 'quizGuru1';
+	response : any;
+	questions : any = [];
+	checkoutForm:any;
+	submitted : boolean = false;
+	analysis = {
+		correct : 0,
+		incorrect : 0
+	};
+
+
+	// public barChartOptions: ChartOptions = {
+	// 	responsive: true,
+	// 	scales: {
+ //            yAxes: [{
+ //                ticks: {
+ //                    beginAtZero: true,
+ //                    maxTicksLimit: 10
+ //                }
+ //            }]
+ //        }
+	// };
+	public barChartOptions: ChartOptions = {
+	    // We use these empty structures as placeholders for dynamic theming.
+	    scales: { xAxes: [{}], 
+	    		  yAxes: [{
+					    	ticks: {
+				                    beginAtZero: true,
+				                    maxTicksLimit: 10
+				                }
+						    }
+	    				] 
+				},
+	    plugins: {
+	      datalabels: {
+	        anchor: 'end',
+	        align: 'end',
+	      }
+	    },
+	    legend: {
+	        display: false
+	    },
+	    maintainAspectRatio: false,
+	    responsive : true
+	  };
+	  public barChartLabels: any[] = ['Incorrect', 'correct'];
+	  public barChartType: ChartType = 'bar';
+	  public barChartLegend = true;
+	  public barChartPlugins = [];
+
+	  public barChartData: ChartDataSets[] = [
+	    { data: [0,0],
+		  backgroundColor: ["red", "blue", "green", "blue", "red", "blue"],
+		  hoverBackgroundColor : ["red", "blue", "green", "blue", "red", "blue"]}
+	  ];
+
+	displayChart : boolean = true;
+
+	constructor(
+		private _app: AppService,
+	    private formBuilder: FormBuilder,
+	) { 
+		this.checkoutForm = this.formBuilder.group({});
+	}
+
+	ngOnInit() {
+		this._app.getShippingPrices().subscribe(
+			res=>{
+				this.response = res;
+				var cnt = 1 , options = {};
+				for(var i in res){
+					options['answer-'+cnt] = new FormControl('', [Validators.required]);
+					cnt++;
+				}
+				this.questions = res;
+				this.checkoutForm = this.formBuilder.group(options);
+				console.log(this.checkoutForm);
+			}
+		);
+	}
+
+	onSubmit(form){
+		this.analysis.correct = 0;
+		this.analysis.incorrect = 0;
+		this.submitted = true;
+		if(form.status == 'INVALID'){
+			console.log(form.status);
+		}else{
+			console.log('Now u can show the graph',form.value);
+			this.questions.forEach(function(a,i,b){
+				a.options.forEach(function(l){
+					if(l.key == form.value['answer-'+(i+1)]){
+						console.log(l);
+						if(l.correct){
+							this.analysis.correct++;
+						}else{
+							this.analysis.incorrect++;
+						}
+					}
+				}.bind(this))
+			}.bind(this)) 
+			console.log(this.analysis);
+			this.barChartData[0].data = [this.analysis.correct,this.analysis.incorrect]
+			this.displayChart = true;
+		}
+	}
+	clearResults(){
+		this.analysis.correct = 0;
+		this.analysis.incorrect = 0;
+		this.barChartData[0].data = [0,0];
+		this.displayChart = true;
+		this.submitted = false;
+
+		var cnt = 1,options = {};
+		for(var i in this.response){
+			options['answer-'+cnt] = new FormControl('', [Validators.required]);
+			cnt++;
+		}
+		this.questions = this.response;
+		this.checkoutForm = this.formBuilder.group(options);
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule , ReactiveFormsModule } from '@angular/forms'
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
